391afe92dfeb1e18b7adae9d70e6d5010dcf8d4e
laurentalacoque <laurent.alacoque@gmail.com>
2019-02-09 16:31:27 +0100

    activate diffusion server

--- poll.py
+++ poll.py
@@ -2,15 +2,15 @@ import aqara
 import json
 import logging
 logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("aqara_devices").setLevel(logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("aqara_devices").setLevel(logging.DEBUG)
 import aqara_devices as AD
 
 root = AD.AqaraRoot()
 def handle_packet(address,kind,data):
     root.handle_packet(data)
 
-connector = aqara.AquaraConnector(data_callback=handle_packet)
+connector = aqara.AquaraConnector(start_server=True,data_callback=handle_packet)
 
 import time
 while(True):
